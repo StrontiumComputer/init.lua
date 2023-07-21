a55e74ade8ae06e30b7a339570878c9b24a06c03
Srijan Ray <srijan@Srijans-MacBook-Pro.local>
2023-07-21 00:09:25 -0400

    Changed harpoon keymaps to my preferences

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -5,6 +5,18 @@ vim.keymap.set("n", "<leader>h", mark.add_file)
 vim.keymap.set("n", "<c-e>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<c-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<c-t>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<c-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<c-s>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<c-i>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<c-o>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<c-p>", function() ui.nav_file(4) end)
+
+
+
+
+
+
+
+
+
+
+
+
