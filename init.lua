0befc0259f3a934d16ae29ccdf8a577b28afb7e9
Srijan Ray <srijanray12@gmail.com>
2023-07-28 22:23:56 -0400

    added flash.nvim and TODO.nvim

--- init.lua
+++ init.lua
@@ -95,6 +95,29 @@ require("lazy").setup({
 	{ "ThePrimeagen/vim-be-good" },
 	{ "mfussenegger/nvim-lint" },
 	{ "mhartington/formatter.nvim" },
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+  -- stylua: ignore
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        },
+	},
 })
 
 vim.cmd([[colorscheme tokyonight]])
