e154343eb4796562fc76824fc6fa8f0a04951b43
Srijan Ray <srijanray12@gmail.com>
2023-08-02 17:20:18 -0400

    removed flash.nvim, added vim surrounnd, made some more ui changes

--- init.lua
+++ init.lua
@@ -90,6 +90,7 @@ require("lazy").setup({
 	},
 	{ "lukas-reineke/indent-blankline.nvim" },
 	{ "saadparwaiz1/cmp_luasnip" },
+	{ "hrsh7th/cmp-path" },
 	{ "nvim-tree/nvim-web-devicons" },
 	{ "norcalli/nvim-colorizer.lua" },
 	{ "ThePrimeagen/vim-be-good" },
@@ -104,20 +105,7 @@ require("lazy").setup({
 			-- refer to the configuration section below
 		},
 	},
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		---@type Flash.Config
-		opts = {},
-  -- stylua: ignore
-        keys = {
-            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-            { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-        },
-	},
+	{ "tpope/vim-surround" },
 })
 
-vim.cmd([[colorscheme tokyonight]])
+vim.cmd([[colorscheme tokyonight-storm]])
