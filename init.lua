9ee0c6a174ff30d40539c3244f6dc5f47a13845e
Srijan Ray <srijanray12@gmail.com>
2023-08-02 22:02:30 -0400

    made a startup screen with alpha.nvim, also added nvim sessions

--- init.lua
+++ init.lua
@@ -86,7 +86,7 @@ require("lazy").setup({
 		"numToStr/Comment.nvim",
 		config = function()
 			require("Comment").setup()
-        end,
+		end,
 	},
 	{ "lukas-reineke/indent-blankline.nvim" },
 	{ "saadparwaiz1/cmp_luasnip" },
@@ -107,6 +107,80 @@ require("lazy").setup({
 	},
 	{ "tpope/vim-surround" },
 	{ "nvim-treesitter/nvim-treesitter-context" },
+	{
+		"goolord/alpha-nvim",
+		event = "VimEnter",
+		opts = function()
+			local dashboard = require("alpha.themes.dashboard")
+			local logo = [[
+
+                          ,%%%%%%%,
+                        ,%%/\%%%%/\%,
+                       ,%%%\c "" J/%%,
+  %.                   %%%%/ d  b \%%%
+  `%%.         __      %%%%    _  |%%%
+   `%%      .-'  `"~--"`%%%%(=_Y_=)%%'
+    //    .'     `.     `%%%%`\7/%%%'____
+   ((    /         ;      `%%%%%%%'____)))
+   `.`--'         ,'   _,`-._____`-,
+     `"""`._____  `--,`          `)))
+                `~"-)))
+      ]]
+
+			dashboard.section.header.val = vim.split(logo, "\n")
+			dashboard.section.buttons.val = {
+				dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+				dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+				dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+				dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+				dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+				dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
+				dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+				dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+			}
+			for _, button in ipairs(dashboard.section.buttons.val) do
+				button.opts.hl = "AlphaButtons"
+				button.opts.hl_shortcut = "AlphaShortcut"
+			end
+			dashboard.section.header.opts.hl = "AlphaHeader"
+			dashboard.section.buttons.opts.hl = "AlphaButtons"
+			dashboard.section.footer.opts.hl = "AlphaFooter"
+			dashboard.opts.layout[1].val = 8
+			return dashboard
+		end,
+		config = function(_, dashboard)
+			-- close Lazy and re-open when the dashboard is ready
+			if vim.o.filetype == "lazy" then
+				vim.cmd.close()
+				vim.api.nvim_create_autocmd("User", {
+					pattern = "AlphaReady",
+					callback = function()
+						require("lazy").show()
+					end,
+				})
+			end
+
+			require("alpha").setup(dashboard.opts)
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "LazyVimStarted",
+				callback = function()
+					local stats = require("lazy").stats()
+					local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+					dashboard.section.footer.val = "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+					pcall(vim.cmd.AlphaRedraw)
+				end,
+			})
+		end,
+	},
+	-- Lua
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre", -- this will only start session saving when an actual file was opened
+		opts = {
+			-- add any custom options here
+		},
+	},
 })
 
 vim.cmd([[colorscheme tokyonight-storm]])
