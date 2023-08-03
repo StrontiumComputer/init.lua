3e6d8b85348c96e8eb56849289a91cb7f6c17ea7
Srijan Ray <srijanray12@gmail.com>
2023-08-02 22:10:07 -0400

    made some visual changes to startup screen

--- init.lua
+++ init.lua
@@ -113,7 +113,6 @@ require("lazy").setup({
 		opts = function()
 			local dashboard = require("alpha.themes.dashboard")
 			local logo = [[
-
                           ,%%%%%%%,
                         ,%%/\%%%%/\%,
                        ,%%%\c "" J/%%,
@@ -129,9 +128,9 @@ require("lazy").setup({
 
 			dashboard.section.header.val = vim.split(logo, "\n")
 			dashboard.section.buttons.val = {
-				dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-				dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-				dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+				dashboard.button("f", "󰱼 " .. " Find file", ":Telescope find_files <CR>"),
+				dashboard.button("n", "󰣕 " .. " New file", ":ene <BAR> startinsert <CR>"),
+				dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
 				dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
 				dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
 				dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
