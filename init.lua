da099d354e24bdd44c771c6d3101b2fb17f8f730
Srijan Ray <srijanray12@gmail.com>
2023-08-05 20:29:42 -0400

    put file explorer button in startup menu

--- init.lua
+++ init.lua
@@ -131,6 +131,7 @@ require("lazy").setup({
 				dashboard.button("f", "󰱼 " .. " Find file", ":Telescope find_files <CR>"),
 				dashboard.button("n", "󰣕 " .. " New file", ":ene <BAR> startinsert <CR>"),
 				dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+				dashboard.button("p", " " .. " File Explorer", ":Ex <CR>"),
 				dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
 				dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
 				dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
