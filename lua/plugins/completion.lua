f936766b0a052b781212681dc3567d362ba89430
Srijan Ray <srijanray12@gmail.com>
2023-12-27 16:35:07 -0500

    fix(completion): Restored Tab key for scrolling through completion menu

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -9,6 +9,12 @@ return {
             require("luasnip.loaders.from_vscode").lazy_load()
             local lspkind = require("lspkind")
             local luasnip = require("luasnip")
+            local has_words_before = function()
+                unpack = unpack or table.unpack
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0 and
+                    vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
 
             cmp.setup({
                 window = {
@@ -75,6 +81,29 @@ return {
                     ["<c-n>"] = cmp.mapping.scroll_docs(-4),
                     ["<c-p>"] = cmp.mapping.scroll_docs(4),
                     ["<c-Space>"] = cmp.mapping.complete({}),
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                            -- that way you will only jump inside the snippet region
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        elseif has_words_before() then
+                            cmp.complete()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
                     ["<CR>"] = cmp.mapping.confirm({
                         behavior = cmp.ConfirmBehavior.Replace,
                         select = true,
@@ -86,8 +115,8 @@ return {
                     end,
                 },
                 sources = cmp.config.sources({
-                    { name = "nvim_lsp", max_item_count = 5 },
                     { name = "luasnip",  max_item_count = 3 },
+                    { name = "nvim_lsp", max_item_count = 5 },
                     { name = "path",     max_item_count = 2 },
                     { name = "nvim_lua", max_item_count = 2 },
                 }, {
