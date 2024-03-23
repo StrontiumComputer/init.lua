6df2d92cbaf6969d95b6e185d946cfc29a7e5063
Srijan Ray <srijanray12@gmail.com>
2024-03-23 13:53:10 -0400

    fix(linting): removed some comments from code

--- lua/plugins/lintandformat.lua
+++ lua/plugins/lintandformat.lua
@@ -5,9 +5,6 @@ return {
         opts = {
             notify_on_error = false,
             format_on_save = function(bufnr)
-                -- Disable "format_on_save lsp_fallback" for languages that don't
-                -- have a well standardized coding style. You can add additional
-                -- languages here or re-enable it for the disabled ones.
                 local disable_filetypes = { c = true, cpp = true }
                 return {
                     timeout_ms = 500,
@@ -16,14 +13,11 @@ return {
             end,
             formatters_by_ft = {
                 lua = { "stylua" },
-                -- Conform can also run multiple formatters sequentially
                 python = { "black" },
                 java = { "astyle" },
                 latex = { "latexindent" },
                 go = { "gofumpt", "goimports-reviser", "golines" },
                 markdown = { "cbfmt" },
-                -- You can use a sub-list to tell conform to run *until* a formatter
-                -- is found.
                 javascript = { { "biome", "prettierd", "prettier" } },
             },
         },
