d0f89c292949e48f7331e3a977f8e4b0e3e4adf7
Srijan Ray <srijanray12@gmail.com>
2024-03-29 16:02:29 -0400

    fix(ui): update trouble to version 3 beta

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -112,25 +112,41 @@ _|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|
     {
         "folke/trouble.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
+        branch = "dev",
+        keys = {
+            {
+                "<leader>xw",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xb",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>xl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xq",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+        },
         config = function()
-            vim.keymap.set("n", "<leader>xx", function()
-                require("trouble").toggle()
-            end)
-            vim.keymap.set("n", "<leader>xw", function()
-                require("trouble").toggle("workspace_diagnostics")
-            end)
-            vim.keymap.set("n", "<leader>xd", function()
-                require("trouble").toggle("document_diagnostics")
-            end)
-            vim.keymap.set("n", "<leader>xq", function()
-                require("trouble").toggle("quickfix")
-            end)
-            vim.keymap.set("n", "<leader>xl", function()
-                require("trouble").toggle("loclist")
-            end)
-            vim.keymap.set("n", "gR", function()
-                require("trouble").toggle("lsp_references")
-            end)
+            require("trouble").setup()
         end,
     },
     -- {
