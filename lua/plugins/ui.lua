c9f84d6f70874b7f2f0050eeb28dee6aac913da5
Srijan Ray <srijanray12@gmail.com>
2024-06-24 23:23:51 -0400

    fix: Added lazy=true tag to devicons for faster load times

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,6 +1,7 @@
 return {
     {
         "nvim-tree/nvim-web-devicons",
+        lazy = true,
         config = function()
             require("nvim-web-devicons").setup({
                 strict = true,
@@ -225,11 +226,11 @@ _|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|
                 },
                 -- you can enable a preset for easier configuration
                 presets = {
-                    bottom_search = false,        -- use a classic bottom cmdline for search
+                    bottom_search = false, -- use a classic bottom cmdline for search
                     -- command_palette = true,       -- position the cmdline and popupmenu together
                     long_message_to_split = true, -- long messages will be sent to a split
                     -- inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-                    lsp_doc_border = false,       -- add a border to hover docs and signature help
+                    lsp_doc_border = false, -- add a border to hover docs and signature help
                 },
             })
         end,
