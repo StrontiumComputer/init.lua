480a15dbcb779c446ef64a6e748b6b02d330b027
Srijan Ray <srijanray12@gmail.com>
2024-07-28 12:54:18 -0400

    feat(quarto): Added quarto.nvim for ipynb programming

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,143 +1,146 @@
 return {
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            { "onsails/lspkind.nvim" },
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "hrsh7th/cmp-path" },
-            { "saadparwaiz1/cmp_luasnip" },
-            { "L3MON4D3/LuaSnip" },
-            { "rafamadriz/friendly-snippets" },
-            { "hrsh7th/cmp-buffer" },
-            { "hrsh7th/cmp-nvim-lua" },
-        },
-        event = "InsertEnter",
-        config = function()
-            local cmp = require("cmp")
-            require("luasnip.loaders.from_vscode").lazy_load()
-            local lspkind = require("lspkind")
-            local luasnip = require("luasnip")
-            local has_words_before = function()
-                unpack = unpack or table.unpack
-                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-                return col ~= 0 and
-                    vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-            end
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			{ "onsails/lspkind.nvim" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-nvim-lua" },
+		},
+		event = "InsertEnter",
+		config = function()
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
+			local lspkind = require("lspkind")
+			local luasnip = require("luasnip")
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
 
-            cmp.setup({
-                window = {
-                    completion = cmp.config.window.bordered({
-                        winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
-                    }),
-                    documentation = cmp.config.window.bordered(),
-                },
-                formatting = {
-                    fields = { "kind", "abbr", "menu" },
-                    format = function(entry, vim_item)
-                        local kind = lspkind.cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
-                        local strings = vim.split(kind.kind, "%s", { trimempty = true })
-                        kind.kind = " " .. (strings[1]) .. "  "
-                        kind.menu = "    " .. (strings[2] or "") .. " "
+			cmp.setup({
+				window = {
+					completion = cmp.config.window.bordered({
+						winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
+					}),
+					documentation = cmp.config.window.bordered(),
+				},
+				formatting = {
+					fields = { "kind", "abbr", "menu" },
+					format = function(entry, vim_item)
+						local kind = lspkind.cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+						local strings = vim.split(kind.kind, "%s", { trimempty = true })
+						kind.kind = " " .. strings[1] .. "  "
+						kind.menu = "    " .. (strings[2] or "") .. " "
 
-                        -- Customization for Pmenu
-                        vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#313244", fg = "NONE" })
-                        vim.api.nvim_set_hl(0, "Pmenu", { fg = "#C5CDD9", bg = "#22252A" })
-                        vim.api.nvim_set_hl(0, "BorderBG", { fg = "#82AAFF", bg = "NONE" })
+						-- Customization for Pmenu
+						vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#313244", fg = "NONE" })
+						vim.api.nvim_set_hl(0, "Pmenu", { fg = "#C5CDD9", bg = "#22252A" })
+						vim.api.nvim_set_hl(0, "BorderBG", { fg = "#82AAFF", bg = "NONE" })
 
-                        vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated",
-                            { fg = "#7E8294", bg = "NONE", strikethrough = true })
-                        vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
-                        vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
-                        vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#A6DA95", bg = "NONE", italic = false })
+						vim.api.nvim_set_hl(
+							0,
+							"CmpItemAbbrDeprecated",
+							{ fg = "#7E8294", bg = "NONE", strikethrough = true }
+						)
+						vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
+						vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
+						vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#A6DA95", bg = "NONE", italic = false })
 
-                        vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = "#EED8DA", bg = "#B5585F" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindProperty", { fg = "#EED8DA", bg = "#B5585F" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindEvent", { fg = "#EED8DA", bg = "#B5585F" })
+						vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = "#EED8DA", bg = "#B5585F" })
+						vim.api.nvim_set_hl(0, "CmpItemKindProperty", { fg = "#EED8DA", bg = "#B5585F" })
+						vim.api.nvim_set_hl(0, "CmpItemKindEvent", { fg = "#EED8DA", bg = "#B5585F" })
 
-                        vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = "#C3E88D", bg = "#9FBD73" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = "#C3E88D", bg = "#9FBD73" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = "#C3E88D", bg = "#9FBD73" })
+						vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = "#C3E88D", bg = "#9FBD73" })
+						vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = "#C3E88D", bg = "#9FBD73" })
+						vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = "#C3E88D", bg = "#9FBD73" })
 
-                        vim.api.nvim_set_hl(0, "CmpItemKindConstant", { fg = "#FFE082", bg = "#D4BB6C" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindConstructor", { fg = "#FFE082", bg = "#D4BB6C" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindReference", { fg = "#FFE082", bg = "#D4BB6C" })
+						vim.api.nvim_set_hl(0, "CmpItemKindConstant", { fg = "#FFE082", bg = "#D4BB6C" })
+						vim.api.nvim_set_hl(0, "CmpItemKindConstructor", { fg = "#FFE082", bg = "#D4BB6C" })
+						vim.api.nvim_set_hl(0, "CmpItemKindReference", { fg = "#FFE082", bg = "#D4BB6C" })
 
-                        vim.api.nvim_set_hl(0, "CmpItemKindFunction", { fg = "#EADFF0", bg = "#A377BF" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindStruct", { fg = "#EADFF0", bg = "#A377BF" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindClass", { fg = "#EADFF0", bg = "#A377BF" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindModule", { fg = "#EADFF0", bg = "#A377BF" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindOperator", { fg = "#EADFF0", bg = "#A377BF" })
+						vim.api.nvim_set_hl(0, "CmpItemKindFunction", { fg = "#EADFF0", bg = "#A377BF" })
+						vim.api.nvim_set_hl(0, "CmpItemKindStruct", { fg = "#EADFF0", bg = "#A377BF" })
+						vim.api.nvim_set_hl(0, "CmpItemKindClass", { fg = "#EADFF0", bg = "#A377BF" })
+						vim.api.nvim_set_hl(0, "CmpItemKindModule", { fg = "#EADFF0", bg = "#A377BF" })
+						vim.api.nvim_set_hl(0, "CmpItemKindOperator", { fg = "#EADFF0", bg = "#A377BF" })
 
-                        vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = "#C5CDD9", bg = "#7E8294" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = "#C5CDD9", bg = "#7E8294" })
+						vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = "#C5CDD9", bg = "#7E8294" })
+						vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = "#C5CDD9", bg = "#7E8294" })
 
-                        vim.api.nvim_set_hl(0, "CmpItemKindUnit", { fg = "#F5EBD9", bg = "#D4A959" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindSnippet", { fg = "#F5EBD9", bg = "#D4A959" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindFolder", { fg = "#F5EBD9", bg = "#D4A959" })
+						vim.api.nvim_set_hl(0, "CmpItemKindUnit", { fg = "#F5EBD9", bg = "#D4A959" })
+						vim.api.nvim_set_hl(0, "CmpItemKindSnippet", { fg = "#F5EBD9", bg = "#D4A959" })
+						vim.api.nvim_set_hl(0, "CmpItemKindFolder", { fg = "#F5EBD9", bg = "#D4A959" })
 
-                        vim.api.nvim_set_hl(0, "CmpItemKindMethod", { fg = "#DDE5F5", bg = "#6C8ED4" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindValue", { fg = "#DDE5F5", bg = "#6C8ED4" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindEnumMember", { fg = "#DDE5F5", bg = "#6C8ED4" })
+						vim.api.nvim_set_hl(0, "CmpItemKindMethod", { fg = "#DDE5F5", bg = "#6C8ED4" })
+						vim.api.nvim_set_hl(0, "CmpItemKindValue", { fg = "#DDE5F5", bg = "#6C8ED4" })
+						vim.api.nvim_set_hl(0, "CmpItemKindEnumMember", { fg = "#DDE5F5", bg = "#6C8ED4" })
 
-                        vim.api.nvim_set_hl(0, "CmpItemKindInterface", { fg = "#D8EEEB", bg = "#58B5A8" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindColor", { fg = "#D8EEEB", bg = "#58B5A8" })
-                        vim.api.nvim_set_hl(0, "CmpItemKindTypeParameter", { fg = "#D8EEEB", bg = "#58B5A8" })
-                        return kind
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<c-n>"] = cmp.mapping.select_next_item(),
-                    ["<c-p>"] = cmp.mapping.select_prev_item(),
-                    ["<c-j>"] = cmp.mapping.scroll_docs(-4),
-                    ["<c-k>"] = cmp.mapping.scroll_docs(4),
-                    ["<c-Space>"] = cmp.mapping.complete({}),
-                    [';j'] = cmp.mapping.confirm({ select = true }),
-                    -- ["<Tab>"] = cmp.mapping(function(fallback)
-                    --     if cmp.visible() then
-                    --         cmp.select_next_item()
-                    --         -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-                    --         -- that way you will only jump inside the snippet region
-                    --     elseif luasnip.expand_or_jumpable() then
-                    --         luasnip.expand_or_jump()
-                    --     elseif has_words_before() then
-                    --         cmp.complete()
-                    --     else
-                    --         fallback()
-                    --     end
-                    -- end, { "i", "s" }),
-                    --
-                    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    --     if cmp.visible() then
-                    --         cmp.select_prev_item()
-                    --     elseif luasnip.jumpable(-1) then
-                    --         luasnip.jump(-1)
-                    --     else
-                    --         fallback()
-                    --     end
-                    -- end, { "i", "s" }),
-                    -- ["<CR>"] = cmp.mapping.confirm({
-                    --     behavior = cmp.ConfirmBehavior.Replace,
-                    --     select = true,
-                    -- }),
-                }),
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp", max_item_count = 9 },
-                    { name = "luasnip",  max_item_count = 3 },
-                    { name = "path",     max_item_count = 2 },
-                    { name = "nvim_lua", max_item_count = 2 },
-                    { name = "buffer",   keyword_length = 3 },
-                }, {
-                }),
-            })
+						vim.api.nvim_set_hl(0, "CmpItemKindInterface", { fg = "#D8EEEB", bg = "#58B5A8" })
+						vim.api.nvim_set_hl(0, "CmpItemKindColor", { fg = "#D8EEEB", bg = "#58B5A8" })
+						vim.api.nvim_set_hl(0, "CmpItemKindTypeParameter", { fg = "#D8EEEB", bg = "#58B5A8" })
+						return kind
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<c-n>"] = cmp.mapping.select_next_item(),
+					["<c-p>"] = cmp.mapping.select_prev_item(),
+					["<c-j>"] = cmp.mapping.scroll_docs(-4),
+					["<c-k>"] = cmp.mapping.scroll_docs(4),
+					["<c-Space>"] = cmp.mapping.complete({}),
+					[";j"] = cmp.mapping.confirm({ select = true }),
+					-- ["<Tab>"] = cmp.mapping(function(fallback)
+					--     if cmp.visible() then
+					--         cmp.select_next_item()
+					--         -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+					--         -- that way you will only jump inside the snippet region
+					--     elseif luasnip.expand_or_jumpable() then
+					--         luasnip.expand_or_jump()
+					--     elseif has_words_before() then
+					--         cmp.complete()
+					--     else
+					--         fallback()
+					--     end
+					-- end, { "i", "s" }),
+					--
+					-- ["<S-Tab>"] = cmp.mapping(function(fallback)
+					--     if cmp.visible() then
+					--         cmp.select_prev_item()
+					--     elseif luasnip.jumpable(-1) then
+					--         luasnip.jump(-1)
+					--     else
+					--         fallback()
+					--     end
+					-- end, { "i", "s" }),
+					-- ["<CR>"] = cmp.mapping.confirm({
+					--     behavior = cmp.ConfirmBehavior.Replace,
+					--     select = true,
+					-- }),
+				}),
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				sources = cmp.config.sources({
+					{ name = "otter", max_item_count = 9 },
+					{ name = "nvim_lsp", max_item_count = 9 },
+					{ name = "luasnip", max_item_count = 3 },
+					{ name = "path", max_item_count = 2 },
+					{ name = "nvim_lua", max_item_count = 2 },
+					{ name = "buffer", keyword_length = 3 },
+				}, {}),
+			})
 
-            cmp.config.formatting = {
-                format = require("tailwindcss-colorizer-cmp").formatter,
-            }
-        end
-    },
+			cmp.config.formatting = {
+				format = require("tailwindcss-colorizer-cmp").formatter,
+			}
+		end,
+	},
 }
