f39a26a78889c108e55aa310c5bb72a77ab44bbd
Srijan Ray <srijanray12@gmail.com>
2024-08-12 21:40:25 -0400

    feat(markview): Upgrade to latest markview version with hybrid mode

--- lua/plugins/plaintext.lua
+++ lua/plugins/plaintext.lua
@@ -1,27 +1,32 @@
 return {
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        build = "cd app && npm install",
-        init = function()
-            vim.g.mkdp_filetypes = { "markdown" }
-        end,
-        ft = { "markdown" },
-    },
-    {
-        "lervag/vimtex",
-    },
-    {
-        "OXY2DEV/markview.nvim",
+	{
+		"OXY2DEV/markview.nvim",
+		lazy = false,
+		dependencies = {
+			-- You may not need this if you don't lazy load
+			-- Or if the parsers are in your $RUNTIMEPATH
+			"nvim-treesitter/nvim-treesitter",
 
-        dependencies = {
-            -- You may not need this if you don't lazy load
-            -- Or if the parsers are in your $RUNTIMEPATH
-            "nvim-treesitter/nvim-treesitter",
-
-            "nvim-tree/nvim-web-devicons",
-        },
-        opts = {},
-    },
-    { "echasnovski/mini.align", version = false, opts = {} },
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("markview").setup({
+				modes = { "n", "i" }, -- If you are using it in insert mode
+				hybrid_modes = { "i" },
+			})
+		end,
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+	{
+		"lervag/vimtex",
+	},
+	{ "echasnovski/mini.align", version = false, opts = {} },
 }
