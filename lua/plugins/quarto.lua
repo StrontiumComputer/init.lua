acbf2ee038dbab2d21b7295089bb0029b5e8f108
Srijan Ray <srijanray12@gmail.com>
2025-04-01 17:17:40 -0400

    Update to neovim v0.11

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -20,37 +20,37 @@ return {
 			format = "py:hydrogen",
 		}, -- see Options
 	},
-	{
-		"GCBallesteros/NotebookNavigator.nvim",
-		keys = {
-			{
-				"]h",
-				function()
-					require("notebook-navigator").move_cell("d")
-				end,
-			},
-			{
-				"[h",
-				function()
-					require("notebook-navigator").move_cell("u")
-				end,
-			},
-			{ "<leader>X", "<cmd>lua require('notebook-navigator').run_cell()<cr>" },
-			{ "<leader>x", "<cmd>lua require('notebook-navigator').run_and_move()<cr>" },
-		},
-		dependencies = {
-			"echasnovski/mini.comment",
-			"hkupty/iron.nvim", -- repl provider
-			-- "akinsho/toggleterm.nvim", -- alternative repl provider
-			-- "benlubas/molten-nvim", -- alternative repl provider
-			"anuvyklack/hydra.nvim",
-		},
-		event = "VeryLazy",
-		config = function()
-			local nn = require("notebook-navigator")
-			nn.setup({ activate_hydra_keys = "<leader>hk" })
-		end,
-	},
+	-- {
+	-- 	"GCBallesteros/NotebookNavigator.nvim",
+	-- 	keys = {
+	-- 		{
+	-- 			"]h",
+	-- 			function()
+	-- 				require("notebook-navigator").move_cell("d")
+	-- 			end,
+	-- 		},
+	-- 		{
+	-- 			"[h",
+	-- 			function()
+	-- 				require("notebook-navigator").move_cell("u")
+	-- 			end,
+	-- 		},
+	-- 		{ "<leader>X", "<cmd>lua require('notebook-navigator').run_cell()<cr>" },
+	-- 		{ "<leader>x", "<cmd>lua require('notebook-navigator').run_and_move()<cr>" },
+	-- 	},
+	-- 	dependencies = {
+	-- 		"echasnovski/mini.comment",
+	-- 		"hkupty/iron.nvim", -- repl provider
+	-- 		-- "akinsho/toggleterm.nvim", -- alternative repl provider
+	-- 		-- "benlubas/molten-nvim", -- alternative repl provider
+	-- 		"anuvyklack/hydra.nvim",
+	-- 	},
+	-- 	event = "VeryLazy",
+	-- 	config = function()
+	-- 		local nn = require("notebook-navigator")
+	-- 		nn.setup({ activate_hydra_keys = "<leader>hk" })
+	-- 	end,
+	-- },
 	{
 		"echasnovski/mini.hipatterns",
 		event = "VeryLazy",
